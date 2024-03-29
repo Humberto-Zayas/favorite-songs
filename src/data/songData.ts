10aa9926ba67e43ea34f781111f863d51e4d7276
Humberto Zayas <soundsinhd@gmail.com>
2024-03-29 11:09:55 -0400

    moved css and song data to own file

---
+++ src/data/songData.ts
@@ -0,0 +1,68 @@
+import nervosa2 from '../assets/images/nervosa2.png';
+import warm from '../assets/images/warm.png';
+import hypochondriac from '../assets/images/hypochondriac.png';
+import brakence2 from '../assets/images/brakence2.png';
+import argyle from '../assets/images/argyle.png';
+
+export const favoriteSongs = [
+  {
+    name: 'Warm',
+    id: 1,
+    image: warm,
+    src: ['/music/warm.mp3'],
+    howl: null,
+    reasons: [
+      'I never heard this combo of RnB and glitch, I ruv it',
+      'I love you love Brakence, you have good taste in moosic',
+      'He really has somethign for everyone imo'
+    ]
+  },
+  {
+    name: 'Brakence 2.0 Freestyle',
+    id: 2,
+    image: brakence2,
+    src: ['/music/brakence.mp3'],
+    howl: null,
+    reasons: [
+      'Some of my favorite lines are thrown out on this track',
+      '"Eat fuck and make beats then I fall backwards" - singing my life with his words',
+      '"She said she wanna fuck with a synth nerd from the suburbs" - damn!'
+    ]
+  },
+  {
+    name: 'Argyle',
+    id: 3,
+    image: argyle,
+    src: ['/music/argyle.mp3'],
+    howl: null,
+    reasons: [
+      'Maybe not the best song to put in here to you, but it\'s real',
+      'The torment of dealing with/pushing away love and finding where shit in your life sits and whatnot',
+      'I guess it\'s all part of the story, idk I think too much tho'
+    ]
+  },
+  {
+    name: 'Hypochondriac',
+    id: 4,
+    image: hypochondriac,
+    src: ['/music/hypochondriac.mp3'],
+    howl: null,
+    reasons: [
+      'First of all the entire album is fire and I love every song',
+      'This one is really sweet and if I had to pick one to you this would be it',
+      '<3'
+    ]
+  },
+  {
+    name: 'Nervosa2',
+    id: 5,
+    image: nervosa2,
+    src: ['/music/nervosa2.mp3'],
+    howl: null,
+    reasons: [
+      'This was the first song by him you sent me',
+      'At first I didn\'t like it at all, but now I ruv it',
+      'It always reminds me of when we first started talking more'
+    ]
+  }
+];
--- src/views/CarouselView.vue
+++ src/views/CarouselView.vue
@@ -6,175 +6,53 @@
     </div>
     <Carousel @slide-start="handleSlideStart" @slide-end="handleSlideEnd" @loop="handleLoop" id="gallery"
       :autoplay="22000" :items-to-show="1" :wrap-around="true" :transition="1000" v-model="currentSlide">
-      <Slide
-        :style="`background-image: linear-gradient(0deg, rgba(0,0,0,1) 4%, rgba(0,0,0,0.5) 80%, rgba(0,0,0,0.3) 100%), url(${song.image})`"
-        class="backdrop" v-for="(song, index) in favoriteSongs" :key="song.id">
-
-        <Transition>
-          <h2 style="font-weight: bold" class="carousel__item">{{ index + 1 }}. {{ song.name }}</h2>
-        </Transition>
-        <div style="height: 100px; width: 80%">
+      <template v-for="(song, index) in favoriteSongs" :key="song.id">
+        <Slide
+          :style="`background-image: linear-gradient(0deg, rgba(0,0,0,1) 4%, rgba(0,0,0,0.5) 80%, rgba(0,0,0,0.3) 100%), url(${song.image})`"
+          class="backdrop">
           <Transition>
-            <div v-if="showOne" class="carousel__item">{{ song.reasons[0] }} </div>
+            <h2 style="font-weight: bold" class="carousel__item">{{ index + 1 }}. {{ song.name }}</h2>
           </Transition>
-        </div>
-        <div style="height: 100px; width: 80%">
-          <Transition>
-            <div v-if="showTwo" class="carousel__item">{{ song.reasons[1] }} </div>
-          </Transition>
-        </div>
-        <div style="height: 100px; width: 80%">
-          <Transition>
-            <div v-if="showThree" class="carousel__item">{{ song.reasons[2] }} </div>
-          </Transition>
-        </div>
-      </Slide>
+          <div style="height: 100px; width: 80%">
+            <Transition>
+              <div v-if="showOne" class="carousel__item">{{ song.reasons[0] }} </div>
+            </Transition>
+          </div>
+          <div style="height: 100px; width: 80%">
+            <Transition>
+              <div v-if="showTwo" class="carousel__item">{{ song.reasons[1] }} </div>
+            </Transition>
+          </div>
+          <div style="height: 100px; width: 80%">
+            <Transition>
+              <div v-if="showThree" class="carousel__item">{{ song.reasons[2] }} </div>
+            </Transition>
+          </div>
+        </Slide>
+      </template>
     </Carousel>
 
     <Carousel @slide-start="handleSlideStart" @slide-end="handleSlideEnd" id="thumbnails" :items-to-show="3.5"
       :wrap-around="true" v-model="currentSlide" ref="carousel">
-      <Slide :style="`background-image: url(${slide.image});`" v-for="slide in favoriteSongs"
-        :class="'galla galla' + slide.id" :itemsToShow="3.95" :key="slide.id">
-        <div class="carousel__item" @click="slideTo(slide.id - 1)">{{ slide.name }}</div>
-
-      </Slide>
+      <template v-for="slide in favoriteSongs" :key="slide.id">
+        <Slide :style="`background-image: url(${slide.image});`"
+          :class="'galla galla' + slide.id" :itemsToShow="3.95">
+          <div class="carousel__item" @click="slideTo(slide.id - 1)">{{ slide.name }}</div>
+        </Slide>
+      </template>
     </Carousel>
   </div>
 </template>
- 
-<style scoped>
-.fakeNav {
-  display: flex;
-  position: fixed;
-  z-index: 10;
-  width: 100%;
-  justify-content: space-between;
-  padding: 0.8em;
-  opacity: 0.7;
-}
-
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 0.5s ease;
-}
-
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-}
-
-.backdrop {
-  background-position: bottom left;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  height: 68vh;
-}
-
-.carousel-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  min-height: 95vh;
-  padding-bottom: 3em;
-}
-
-.galla {
-  background-size: cover;
-}
-
-/* #gallery {
-  height: 75vh;
-} */
-
-
-.galla1 {
-  background-color: rgb(0, 115, 255);
-  min-height: 20vh;
-}
-
-.galla2 {
-  background-color: rgb(200, 41, 248);
-  min-height: 20vh;
-}
-
-.galla3 {
-  background-color: rgb(248, 41, 158);
-  min-height: 20vh;
-}
-
-.galla4 {
-  background-color: rgb(248, 75, 41);
-  min-height: 20vh;
-}
-
-.galla5 {
-  background-color: rgb(248, 210, 41);
-  min-height: 20vh;
-}
-
-.carousel__slide {
-  padding: 5px;
-}
-
-.carousel__viewport {
-  perspective: 2000px;
-  height: 100%;
-}
-
-.carousel__track {
-  transform-style: preserve-3d;
-  height: 100%;
-}
-
-.carousel__slide--sliding {
-  transition: 0.5s;
-}
-
-.carousel__slide {
-  opacity: 0.9;
-  transform: scale(0.9);
-  z-index: 0;
-}
-
-.carousel__slide--active~.carousel__slide {
-  /* transform: rotateY(20deg) scale(0.9); */
-  /* z-index: 10; */
-}
-
-.carousel__slide--prev {
-  opacity: 1;
-  transform: scale(0.95);
-}
-
-.carousel__slide--next {
-  opacity: 1;
-  transform: scale(0.95);
-}
-
-.carousel__slide--active {
-  opacity: 1;
-  transform: scale(1.14);
-  z-index: 1;
-  box-shadow: 0px 0px 10px 10px rgba(0, 0, 0, 0.56);
-}
-</style>
 
 <script lang="ts">
-import { Howl, Howler } from 'howler';
+import { defineComponent, ref, computed, onMounted, onUnmounted } from 'vue'
+import { Howl } from 'howler'
 import start from '../assets/images/power.svg'
 import finish from '../assets/images/log-out.svg'
-import { defineComponent, Transition } from 'vue'
 import { Carousel, Slide } from 'vue3-carousel'
-
-import nervosa2 from '../assets/images/nervosa2.png'
-import warm from '../assets/images/warm.png'
-import hypochondriac from '../assets/images/hypochondriac.png'
-import brakence2 from '../assets/images/brakence2.png'
-import argyle from '../assets/images/argyle.png'
-
+import { favoriteSongs } from '../data/songData'
 import 'vue3-carousel/dist/carousel.css'
-import router from '@/router';
+import '@/assets/css/GalleryStyles.css';
 
 export default defineComponent({
   name: 'Gallery',
@@ -182,159 +60,100 @@ export default defineComponent({
     Carousel,
     Slide,
   },
-  data: () => ({
-    start: start,
-    finish: finish,
-    show: false,
-    showOne: false,
-    showTwo: false,
-    showThree: false,
-    currentSlide: 0,
-    currentSong: null,
-    previousSong: '',
-    favoriteSongs: [
-      {
-        name: 'Warm',
-        id: 1,
-        image: warm,
-        src: ['/music/warm.mp3'],
-        howl: null,
-        reasons: [
-          'I never heard this combo of RnB and glitch, I ruv it',
-          'I love you love Brakence, you have good taste in moosic',
-          'He really has somethign for everyone imo'
-        ]
-      },
-      {
-        name: 'Brakence 2.0 Freestyle',
-        id: 2,
-        image: brakence2,
-        src: ['/music/brakence.mp3'],
-        howl: null,
-        reasons: [
-          'Some of my favorite lines are thrown out on this track',
-          '"Eat fuck and make beats then I fall backwards" - singing my life with his words',
-          '"She said she wanna fuck with a synth nerd from the suburbs" - damn!'
-        ]
-      },
-      {
-        name: 'Argyle',
-        id: 3,
-        image: argyle,
-        src: ['/music/argyle.mp3'],
-        howl: null,
-        reasons: [
-          'Maybe not the best song to put in here to you, but it\'s real',
-          'The torment of dealing with/pushing away love and finding where shit in your life sits and whatnot',
-          'I guess it\'s all part of the story, idk I think too much tho'
-        ]
-      },
-      {
-        name: 'Hypochondriac',
-        id: 4,
-        image: hypochondriac,
-        src: ['/music/hypochondriac.mp3'],
-        howl: null,
-        reasons: [
-          'First of all the entire album is fire and I love every song',
-          'This one is really sweet and if I had to pick one to you this would be it',
-          '<3'
-        ]
-      },
-      {
-        name: 'Nervosa2',
-        id: 5,
-        image: nervosa2,
-        src: ['/music/nervosa2.mp3'],
-        howl: null,
-        reasons: [
-          'This was the first song by him you sent me',
-          'At first I didn\'t like it at all, but now I ruv it',
-          'It always reminds me of when we first started talking more'
-        ]
-      }
-    ]
-  }),
-  methods: {
-    handleLoop() {
-      console.log('Looping');
-      this.currentTrack.stop();
-      this.$router.push('/about')
-    },
-    resetShows() {
-      this.showOne = false;
-      this.showTwo = false;
-      this.showThree = false;
-    },
-    startShows() {
-      setTimeout(() => {
-        this.showOne = true
-      }, 2000)
+  setup() {
+    const startSrc = start;
+    const finishSrc = finish;
 
-      setTimeout(() => {
-        this.showTwo = true
-      }, 6000)
+    const showOne = ref(false);
+    const showTwo = ref(false);
+    const showThree = ref(false);
+    const currentSlide = ref(0);
 
-      setTimeout(() => {
-        this.showThree = true
-      }, 11000)
-    },
-    slideTo(val: number) {
-      this.currentTrack.stop();
-      this.currentSlide = val
-    },
-    handleSlideStart(data: any) {
-
-      this.currentTrack.stop()
-      this.resetShows();
-    },
-    handleSlideEnd(data: any) {
-      this.startShows();
-      if (!this.currentTrack.playing()) {
-        console.log(this.currentTrack.playing())
-
-
-        this.currentTrack.play()
-
-      }
-
-      this.startShows()
-
-
-    }
-  },
-  mounted: function () {
-    this.currentTrack.play();
-    setTimeout(() => {
-      this.showOne = true
-    }, 1000)
-
-    setTimeout(() => {
-      this.showTwo = true
-    }, 5000)
-
-    setTimeout(() => {
-      this.showThree = true
-    }, 10000)
-
-  },
-  // onDestroy: function () {
-  //   this.currentTrack.stop()
-  //   this.currentSlide = 0
-  // },
-  unmounted: function () {
-    console.log('unmounting')
-    this.currentTrack.stop()
-    this.currentSlide = 0
-  },
-  computed: {
-    currentTrack() {
-      var sound = new Howl({
-        src: this.favoriteSongs[this.currentSlide].src,
+    const currentTrack = computed(() => {
+      return new Howl({
+        src: favoriteSongs[currentSlide.value].src,
         html5: true,
-      })
-      return sound;
+      });
+    });
+
+    function handleLoop() {
+      console.log('Looping');
+      currentTrack.value.stop();
+      // You may need to adjust this part according to your routing logic
+      // this.$router.push('/about');
     }
-  }
-})
+
+    function resetShows() {
+      showOne.value = false;
+      showTwo.value = false;
+      showThree.value = false;
+    }
+
+    function startShows() {
+      setTimeout(() => {
+        showOne.value = true;
+      }, 2000);
+
+      setTimeout(() => {
+        showTwo.value = true;
+      }, 6000);
+
+      setTimeout(() => {
+        showThree.value = true;
+      }, 11000);
+    }
+
+    function slideTo(val: number) {
+      currentTrack.value.stop();
+      currentSlide.value = val;
+    }
+
+    function handleSlideStart() {
+      currentTrack.value.stop();
+      resetShows();
+    }
+
+    function handleSlideEnd() {
+      startShows();
+      if (!currentTrack.value.playing()) {
+        currentTrack.value.play();
+      }
+    }
+
+    onMounted(() => {
+      currentTrack.value.play();
+      setTimeout(() => {
+        showOne.value = true;
+      }, 1000);
+
+      setTimeout(() => {
+        showTwo.value = true;
+      }, 5000);
+
+      setTimeout(() => {
+        showThree.value = true;
+      }, 10000);
+    });
+
+    onUnmounted(() => {
+      console.log('unmounting');
+      currentTrack.value.stop();
+      currentSlide.value = 0;
+    });
+
+    return {
+      start: startSrc,
+      finish: finishSrc,
+      showOne,
+      showTwo,
+      showThree,
+      currentSlide,
+      handleLoop,
+      handleSlideStart,
+      handleSlideEnd,
+      slideTo,
+      favoriteSongs, // Expose favoriteSongs
+    };
+  },
+});
 </script>
